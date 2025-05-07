7988b2b605af06199c0b3419511c584e7107d8a7
theocayla <theocayla@hotmail.fr>
2025-05-07 11:29:51 +0200

    Dump dataset related information in pipeline results

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -8,6 +8,7 @@ from typing import List
 from dataset import EvaluationDataset
 from engine_evaluation import EngineEvaluator
 from model_evaluation import ModelEvaluator
+from utils import get_dict_types, make_dict_json_compatible
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -61,17 +62,27 @@ class EvaluationPipeline:
         """
         Save results in a json file
         """
-        result_file = f"data/results/{self.run_id}/metrics.json"
+        result_file = f"evaluation/data/results/{self.run_id}/metrics.json"
         os.makedirs(os.path.dirname(result_file), exist_ok=True)
         logging.info(f"Saving metrics in {result_file}")
 
+        dataset_info = {
+                "ID" : self.dataset.dataset_ID,
+                "datapath" : self.dataset.datapath,
+                "Number of Images" : len(self.dataset),
+                "NUmber of Sequences" : len(self.dataset.sequences)
+            }
+
         self.metrics.update({
             "config" : self.config,
-            "run_id" : self.run_id
+            "run_id" : self.run_id,
+            "dataset" : dataset_info
         })
 
+        metrics_dump = make_dict_json_compatible(self.metrics)
+
         with open(result_file, 'w') as fp:
-            json.dump(self.metrics, fp)
+            json.dump(metrics_dump, fp)
 
     def display_metrics(self, subset = ["model", "engine"]):
         def format_metric(value):
@@ -106,6 +117,6 @@ class EvaluationPipeline:
         """
         Generates a unique run_id to store results
         """
-        timestamp = datetime.now().strftime("%Y%m%d")
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M")
         rand_suffix = random.randint(1000, 9999)
         return f"run-{timestamp}-{rand_suffix}"
